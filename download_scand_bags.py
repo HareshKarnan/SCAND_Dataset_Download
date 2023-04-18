ce4a027286d3cc73719177d8513e7c4f57cf4492
Haresh Karnan <haresh.miriyala@gmail.com>
2023-04-18 09:59:11 -0500

    added option to only  download  spot or jackal bags

--- download_scand_bags.py
+++ download_scand_bags.py
@@ -11,16 +11,21 @@ def main():
     parser = argparse.ArgumentParser(description='Download all bags from scand archive.')
     parser.add_argument('--csv_file', type=str, help='Path to csv file.', default='download_links.csv')
     parser.add_argument('--output_dir', type=str, help='Path to output directory.', default='data/')
+    parser.add_argument('--spot_only', action='store_true', help='Only download bags from spot.')
+    parser.add_argument('--jackal_only', action='store_true', help='Only download bags from jackal.')
     args = parser.parse_args()
     
     # check if the data/ directory exists and is empty
     if not os.path.exists(args.output_dir):
+        print('###############################################')
         print('Creating directory {}.'.format(args.output_dir))
+        print('###############################################')
         os.mkdir(args.output_dir)
     else:
         if len(os.listdir(args.output_dir)) > 0:
             print('Directory {} is not empty. Please remove all files and try again.'.format(args.output_dir))
             sys.exit(1)
+        
 
     # download all bags
     with open(args.csv_file, 'r') as f:
@@ -30,6 +35,11 @@ def main():
         for row in reader:
             bag_number = row[0]
             robot_name = row[1]
+            
+            # check if we want to download only spot or jackal bags
+            if args.spot_only and robot_name != 'spot': continue
+            if args.jackal_only and robot_name != 'jackal': continue
+            
             bag_link = row[2]
             bag_name = '{}_{}.bag'.format(bag_number, robot_name)
             # download the bag file from the bag_link, wait until the download is complete before moving on
